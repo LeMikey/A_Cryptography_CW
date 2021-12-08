bfd21abf449f267445b6d7b5f95804a747c5f05c
nickjayesh <80024255+nickjayesh@users.noreply.github.com>
2021-12-08 17:24:19 +0530

    Update GUI.py

---
+++ GUI.py
@@ -0,0 +1,32 @@
+import sqlite3,hashlib
+from tkinter import *
+
+window = Tk()
+
+window.title("PASSWRLD")
+
+def checkPassword():
+    print("Button clicked!")
+
+#Function to display Login Screen
+def loginScreen():
+    window.geometry("300x200")
+
+    lbl1 = Label(window,text="PASSWRLD")
+    lbl1.pack(pady=30)
+
+    lbl2 = Label(window, text="Enter master password")
+    lbl2.config(anchor=CENTER)
+    lbl2.pack()
+
+    txt = Entry(window,width=20)
+    txt.pack()
+    txt.focus()
+
+
+
+    btn = Button(window, text="Submit",command=checkPassword())
+    btn.pack(pady=10)
+
+loginScreen()
+window.mainloop()
