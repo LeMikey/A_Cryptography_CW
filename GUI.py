c2746a073dbc882e54b87f21c84505ba6093f714
nickjayesh <80024255+nickjayesh@users.noreply.github.com>
2021-12-08 18:42:06 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -5,9 +5,6 @@ window = Tk()
 
 window.title("PASSWRLD")
 
-def checkPassword():
-    print("Button clicked!")
-
 #Function to display Login Screen
 def loginScreen():
     window.geometry("300x200")
@@ -19,14 +16,27 @@ def loginScreen():
     lbl2.config(anchor=CENTER)
     lbl2.pack()
 
-    txt = Entry(window,width=20)
+    txt = Entry(window, width=20, show="*")
     txt.pack()
     txt.focus()
 
+    lbl3 = Label(window)
+    lbl3.pack()
 
+    #Function to check master password
+    def checkPassword():
+        password = "test"
+
+        if password==txt.get():
+            print("Correct Answer!")
+        else:
+            lbl3.config(text="Incorrect Password!")
 
     btn = Button(window, text="Submit",command=checkPassword)
     btn.pack(pady=10)
+
+#Function to display password vault
+
 
 loginScreen()
 window.mainloop()
