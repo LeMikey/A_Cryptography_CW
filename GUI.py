95213e125bda57fdda7516ea574b8f28c5e83b9a
nickjayesh <80024255+nickjayesh@users.noreply.github.com>
2021-12-12 12:58:47 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -3,6 +3,15 @@ import hashlib
 from tkinter import *
 from tkinter import simpledialog
 from functools import partial
+import uuid
+import pyperclip
+import base64
+import os
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+from cryptography.fernet import Fernet
+
 
 # Initiate database
 with sqlite3.connect("PASSWRLD.db") as db:
